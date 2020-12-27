66d143af388c30af37028ac973f30978effa2d9d
Danial422 <76181817+Danial422@users.noreply.github.com>
2020-12-27 23:18:57 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// write a javascript program that gets temperature from user in celsius and convert it into Fahrenheit 
-// using the formula F = 9/5 * C + 32, alert the output. 
-var usertemperature = parseInt(prompt("Enter temperature in celsius"));
-var temperature = "temp";
-var C = usertemperature;
-var fv = 9/5 ;
-var sv = 32 ;
-var F = fv * C + sv ;
-alert(F );
